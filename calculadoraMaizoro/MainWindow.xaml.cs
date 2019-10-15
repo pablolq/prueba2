8b643cd057d0ccdbc3c3c4eff1b0c94adbb42192
dai <dai@itam.mx>
2019-10-15 11:26:10 -0500

    Version inicial

---
+++ calculadoraMaizoro/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculadoraMaizoro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)rbSuma.IsChecked)
+                suma();
+            else
+                resta();
+        }
+
+        public void suma() {
+            txRes.Text = ""+ (Convert.ToDouble(txNum1.Text) + Convert.ToDouble(txNum2.Text)); 
+        }
+
+        public void resta()
+        {
+            txRes.Text = "" + (Convert.ToDouble(txNum1.Text) - Convert.ToDouble(txNum2.Text)); 
+        }
+    }
+}
